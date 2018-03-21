8f590bf2fa15f1c5e41a057f020a1ec0bd2afa73
Enehta <h.mollering@student.utwente.nl>
2018-03-21 14:53:09 +0100

    changed histogram to bar diagram
    changed histogram to bar diagram

--- LBPTest.m
+++ LBPTest.m
@@ -1,12 +1,14 @@
 fingerprint = imread('s1.png');
 greyFingerprint = rgb2gray(fingerprint);
+test = size(greyFingerprint);
+test2 = prod(floor(size(greyFingerprint)./[3 3]));
 
 figure
 imshow(greyFingerprint)
 title('Original Fingerprint')
 
-lbp = extractLBPFeatures(greyFingerprint,'Normalization','None');
+lbpFeatures = extractLBPFeatures(greyFingerprint);
 
 figure
-histogram(lbp);
+bar(lbpFeatures);
 title ('Histogram of s1');
