bfff01ffbcbb42ecbd1079c1b0351a77bee829f6
reka193 <reka193@hotmail.com>
2018-06-25 14:38:09 +0200

    Create filecopy.py

---
+++ filecopy.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+if __name__ == '__main__':
+    n=0
+    for root, dirs, files in os.walk('D:/...'):          # replace the ... with your starting directory
+        for file in files:
+            path_file = os.path.join(root,file)
+            n=n+1
+            shutil.copy(path_file,'D:/.../'+str(n)+'.png')          # replace the ... with the destination dir
